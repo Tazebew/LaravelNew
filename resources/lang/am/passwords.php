c2e44535f252bcf66b46ab991b9f78ad71cf2dd8
Tazebew Kassa <tazebewkassa@Tazebew.local>
2019-06-10 18:22:57 +0300

    Initial Commit

---
+++ resources/lang/am/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'የይለፍ ቃላት ቢያንስ ስምንት ፊደሎች መሆን እና ማረጋገጫውን ማዛመድ አለበት.',
+    'reset' => 'የይለፍ ቃልዎ በድጋሚ ተዘጋጅቷል!',
+    'sent' => 'የይለፍ ቃል ዳግም ማስጀመሪያ አገናኝዎን በኢሜል ልከናል!',
+    'token' => 'ይህ የይለፍ ቃል ዳግም ማስጀመሪያ ተለዋጭ ስም የተሳሳተ ነው.',
+    'user' => "ያንን የኢ-ሜይል አድራሻ የያዘ ተጠቃሚ ማግኘት አልቻልንም.",
+
+];
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{__("Welcome")}}</div>
+
+                    <div class="card-body">
+                        <div class="container text-center">
+                            <h1>@lang("Hi there!")</h1>
+                            <div class="col-md-offset-2">
+                                <p>{{ __("I hope this meets you well.")}}</p>
+                                <p>{{ __("Let's talk about Localization in Laravel, trust me, it's an awesome topic. Notwithstanding, not many have come to realize this awesomeness.")}}</p>
+                                <p>{{ __("Localization according to the dictionary, means adapting something to a fixed position or location. It therefore suffices to say that Laravel localization involves adapting the language used in the app to the given location of the user.")}}</p>
+                            </div>
+                            <p>{{ __("This might not make much sense to you but you've got no reason to worry. Nothing said here should be argued about because the idea is just to make up words for the purpose of testing this tutorial.")}}</p>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+
+
